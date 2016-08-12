820dbec92e4302bf64739c9c7c95270ba4682034
pil0t <kl.vlad@gmail.com>
2016-08-12 19:41:42 +0300

    ftttt

--- GitFlowExperiments/Program.cs
+++ GitFlowExperiments/Program.cs
@@ -15,7 +15,7 @@ namespace GitFlowExperiments
 
             Console.WriteLine("One more New Version Feature!"); // more changes
 
-            Console.WriteLine("Release ONE 11"); // Release changes
+            Console.WriteLine("Release ONE 11"); // Release changes something here
 
             Console.WriteLine("New FeatureTWO!");
         }
