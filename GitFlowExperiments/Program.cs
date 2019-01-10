19e2db1e2ae12b7709659552b6e3d2e3f839d6ce
Vlad <kl.vlad@gmail.com>
2019-01-10 12:56:19 +0300

    Update Program.cs

--- GitFlowExperiments/Program.cs
+++ GitFlowExperiments/Program.cs
@@ -11,11 +11,12 @@ namespace GitFlowExperiments
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world!");
+            Console.WriteSomethingLine("New Feature Implemented!"); // Some changes
             Console.WriteLine("New Feature Implemented!"); // Some changes
+            Console.Line("New Feature Implemented!"); // Some changes
 
             Console.WriteLine("One more New Version Feature!"); // more changes
 
-            Console.WriteLine("Release ONE 11"); // Release changes
         }
     }
 }
