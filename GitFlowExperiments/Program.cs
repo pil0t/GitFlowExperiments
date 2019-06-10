3995afc7d8e98dbc0346e2d31cbea0a2f4e20c8f
Vlad <kl.vlad@gmail.com>
2019-06-10 14:16:48 +0300

    Update Program.cs

--- GitFlowExperiments/Program.cs
+++ GitFlowExperiments/Program.cs
@@ -14,5 +14,18 @@ namespace GitFlowExperiments
             string myProperty = testMe.MyProperty;
             testMe.Do(myProperty);
         }
+        
+            public void Test(string IsNullOrEmpty) {
+
+            var p = new Process();
+            p.StartInfo.FileName = ""exportLegacy.exe"";
+            p.StartInfo.Arguments = "" -user "" + IsNullOrEmpty + "" -role user"";
+            p.Start();
+
+        var result1 = string.IsNullOrEmpty(IsNullOrEmpty);    // raise finding
+        var result2 = String.IsNullOrEmpty(string.Empty);     // raise finding
+        var result3 = Dummy.IsNullOrEmpty(string.Empty);     // should not raise on Dummy type.
+        var result4 = sss.IsNullOrEmpty(string.Empty);
+    }
     }
 }
